bcdad7629770d4dbfa05cb6602c8cbef0775d9f3
NaveenJeganathan <159634977+NaveenJeganathan@users.noreply.github.com>
2024-02-26 02:07:31 +0530

    Update SecurityConfig.java

--- src/main/java/com/eventmanagement/config/SecurityConfig.java
+++ src/main/java/com/eventmanagement/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 // this is config
+// check
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
